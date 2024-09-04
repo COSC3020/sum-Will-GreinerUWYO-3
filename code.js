52949d32a1c4bd07870a4f9a666d768629d1c9b9
wgreine2 <144729039+Will-GreinerUWYO@users.noreply.github.com>
2024-09-04 12:39:26 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,6 +1,7 @@
 function sum(a) {
-    var sum = a[0];
-    for(var i = 1; i < a.length; i++) {
+    var sum = 0;
+    sum += a[0];
+    for(var i = 0; i < a.length; i++) {
         sum += a[i];
     }
     return sum;
